cbb815f1b5236e12868e65da4d6822ed2112309d
pevveme <47577706+PinkytheDev@users.noreply.github.com>
2019-05-18 00:28:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import time
 import aiohttp
 import json
 import os
-from discord import Game
+from discord.ext import Game
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='.m')
